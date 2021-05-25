eb952ae3c054921c5ac713027c49571aad1c5728
Marcos Derós <marcosderos78@gmail.com>
2021-05-25 14:25:35 -0300

    Noise removed

--- scripts/Generate_single_proton_events.sh
+++ scripts/Generate_single_proton_events.sh
@@ -17,7 +17,7 @@ do
   mkdir $j;
   cd $j;
   o2-sim -m MFT -e TGeant3 -g boxgen -n $events --configKeyValues 'BoxGun.pdg=2212 ; BoxGun.eta[0]=-20 ; BoxGun.eta[1]=-20.0; BoxGun.number=1; Diamond.position[0]=0.25;Diamond.position[1]=4.004;Diamond.position[2]=-45.000;Diamond.width[2]=0; MFTBase.buildHeatExchanger=false; MFTBase.buildFlex=false';
-  o2-sim-digitizer-workflow -b --skipDet TPC,ITS,TOF,FT0,EMC,HMP,ZDC,TRD,MCH,MID,FDD,PHS,FV0,CPV --interactionRate $j;
+  o2-sim-digitizer-workflow -b --skipDet TPC,ITS,TOF,FT0,EMC,HMP,ZDC,TRD,MCH,MID,FDD,PHS,FV0,CPV --interactionRate $j --configKeyValues "MFTDigitizerParam.noisePerPixel=0";
   o2-mft-reco-workflow -b;
   cd ..;
 done
