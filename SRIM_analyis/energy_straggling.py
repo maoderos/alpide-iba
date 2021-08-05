f06a82f3d4a0c2fc4f0e0b23de78fad054195e38
marcosderos <marcosderos78@gmail.com>
2021-08-05 01:05:59 -0300

    Finished data array

--- SRIM_analyis/energy_straggling.py
+++ SRIM_analyis/energy_straggling.py
@@ -1,49 +1,74 @@
+# TODO: Test interpolation to see if 15um file is necessary 
+# Correct the sort problem (DONE)
+# Study interpolation in python and types
+# If yes -> Integral for 15 + 3sigma and 15 - 3sigma -> Get Sigma from file (?)
+# If no -> Get values, insert into list, interpolate to straggling and integrate 
+
+
 import os
 import sys
 import re
 import numpy as np
 from io import StringIO 
+import matplotlib.pyplot as plt
 
 #Function to get line with 15um
 
-def get_15um_line(file): # get line with data from 15um from SRIM table
-    for line in file:
-        if re.search("15,00 um",line):
-            return line
-        elif re.search("15,01 um",line):
-            return line 
+def line_filter(line):
+    line1 = line.replace(" keV", "E-03")
+    line2 = line1.replace(" MeV", "")
+    line3 = line2.replace(" A", "E-04")
+    line4 = line3.replace(" um", "")
+    line5 = line4.replace("   ", " ")
+    line6 = line5.replace("  "," ")
+    line7 = line6.replace("  "," ")
+    line8 = line7.replace(",",".")
+    line_final = line8[1:]
+    return line_final
 
-def format_file(file,element): # Format SRIM tables and return lists
+
+def get_15um_line(filename): # get line with data from 15um from SRIM table
+    file = open("15um_values/{0}".format(filename), "r") 
+    for line in file:
+        if re.search("15,00 um",line) or re.search("15,01 um",line):
+            line_format = line_filter(line)
+            return line_format
+            
+
+def format_file(filename,element): # Format SRIM tables and return lists
     kinEn =[]; dE_dx = []; range = []; long_strag =[]; lat_strag = [];
+    file = open("tables/{0}".format(filename), "r") 
+    lines_data = []
     for i, line in enumerate(file):
         if i in np.arange(25, 104):
-            line1 = line.replace(" keV", "e-03")
-            line2 = line1.replace(" MeV", "")
-            line3 = line2.replace(" A", "e-04")
-            line4 = line3.replace(" um", "")
-            line5 = line4.replace("   ", " ")
-            line6 = line5.replace("  "," ")
-            line7 = line6.replace("  "," ")
-            line8 = line7.replace(",",".")
-            line_final = StringIO(line8)
-            # use numpy to separate formated line and append to lists above
-            E, dE_dx_elec, dE_dx_nuc, proj_rang, long_straggling, lat_straggling = np.loadtxt(line_final, unpack=True)
-            kinEn.append(E); range.append(proj_rang); long_strag.append(long_straggling); lat_strag.append(lat_straggling)
-            dE_dx_sum = np.add(dE_dx_elec,dE_dx_nuc) #sum the stopping powers
-            dE_dx.append(dE_dx_sum)
-    return kinEn, dE_dx, range, long_strag, lat_strag
+            lines_data.append(line_filter(line)) 
+    # After getting a list of lines of data, get the line of 15um, add it to list and sort it.
+    lines_data.append(get_15um_line(filename))
+    #lines_data.append(line_format_15)
+    data_array_unsort = np.loadtxt(lines_data)
+    #sort numpy array by the first collumn
+    data_array = data_array_unsort[np.argsort(data_array_unsort[:, 0])]
+    
+    return data_array
 
-#elements = ["H", "He", "Li", "Be", "B", "C", "N", "O", "F", "Ne"]
-elements = ["H"]
+
+elements = ["H", "He", "Li", "Be", "B", "C", "N", "O", "F", "Ne"]
+#elements = ["H"]
 radical = "Al-Si-O.txt"
 
 
 for element in elements: #Loop in all files of folder
     filename = "{0}_{1}".format(element, radical)
-    generic_file = open("tables/{0}".format(filename), "r")
-    kinEn, dE_dx, range, long_strag, lat_strag = format_file(generic_file, element) # format file and tranform data indo lists
-    # TODO: Test interpolation to see if 15 file is necessary 
-    # Study interpolation in python and types
-    # If yes -> Integral for 15 + 3sigma and 15 - 3sigma -> Get Sigma from file (?)
-    # If no -> Get values, insert into list, interpolate to straggling and integrate 
+    #kinEn, dE_dx, range, long_strag, lat_strag = format_file(filename, element) # format file and tranform data indo lists
+    data_arr = format_file(filename,element)
+    #kinEn_15, dE_dx_15, long_strag_15, lat_strag_15 = get_15um_line(filename)
+
+
+    #plt.xlabel("range(um)")
+    #plt.ylabel("dE/dx (keV/um)")
+    #plt.xlim(0,20)
+    #plt.plot(range,dE_dx)
+    #plt.show()
+
+
 
