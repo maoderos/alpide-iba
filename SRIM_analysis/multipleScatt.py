4073b4d4c5d97e0dcaaf607080c800d747dcc95a
Marcos Derós <marcosderos78@gmail.com>
2021-11-13 10:55:14 -0300

    Error in unity fixed, changed output to degress

--- SRIM_analysis/multipleScatt.py
+++ SRIM_analysis/multipleScatt.py
@@ -23,9 +23,9 @@ def getRadiation_length(Xo, weigth, density):
 def getMultipleScatt(Ek_initial, loss_fraction,ion_mass,z,x, radiation_length, weigth, density):
     # We will use the mean energy as a first approximation
     Ek_mean = (Ek_initial + Ek_initial*loss_fraction)/2
-    E_rest = ion_mass
+    E_rest = ion_mass # cause mass is in MeV/c**2
     E_mean = Ek_mean + E_rest
-    # The momentum using the spectial relativity
+    # The momentum using special relativity
     p = np.sqrt(E_mean**2 - E_rest**2) #MeV/c
     # Need to calculate beta using special relativity
     xi = (Ek_mean + E_rest)/E_rest
@@ -33,7 +33,7 @@ def getMultipleScatt(Ek_initial, loss_fraction,ion_mass,z,x, radiation_length, w
 
     Xo_mix = getRadiation_length(radiation_length, weigth, density)
 
-    return (13.6/(beta*p))*z*np.sqrt(x/Xo_mix)*(1 + 0.038*np.log((x*z**2)/(Xo_mix*beta**2) ) )
+    return (13.6/(beta*p))*z*np.sqrt((x*1e-4)/Xo_mix)*(1 + 0.038*np.log((x*z**2)/(Xo_mix*beta**2) ) )*57.2957
     
 
 
