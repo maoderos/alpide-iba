7c2c6e28f224548bcac6f173d03f4c78d233e0a8
Marcos Derós <marcosderos78@gmail.com>
2021-12-17 20:28:08 -0300

    added more detailed energy loss calculation

--- SRIM_analysis/SRIM_analysis.py
+++ SRIM_analysis/SRIM_analysis.py
@@ -24,9 +24,13 @@ EnergyLossTable = True
 
 #----ALPIDE parameters and materials
 thickness = 50.0 #in um
-density = 2.313 # g/cm3
+density_alpide = 2.313 # g/cm3
+density_metal = 2.28 # g/cm3 
+density_si = 2.32 # g/cme
 radiation_length = [21.82, 24.01, 34.24] # Si,Al,O
-weigth =  [0.8776, 0.034, 0.0884] # Si, Al, O
+weigth_alpide =  [0.8776, 0.034, 0.0884] # Si, Al, O
+weigth_metal = [0.388, 0.17, 0.442] 
+weigth_si = [1.0]
 
 table_totalEloss = []
 table_elecEloss = []
@@ -57,22 +61,24 @@ def get_thick_line(filename,value): # get line with data of thickness from SRIM 
             return line_format
             
 
-def format_file(filename,element): # Format SRIM tables and return lists
+def format_file(filename,element,get_thick=True,init=25,end=130): # Format SRIM tables and return lists
     file = open("tables/{0}".format(filename), "r") 
     lines_data = []
     for i, line in enumerate(file):
-        if i in np.arange(25, 130): 
+        if i in np.arange(init, end): 
             lines_data.append(line_filter(line)) 
     # After getting a list of lines of data, get the line of thickness, add it to list and sort it.
-    line_special = get_thick_line(filename,thickness)
-    repeated_line = False
-    for j in lines_data:
-        if j == line_special:
-            repeated_line = True
+    if(get_thick):
+    
+        line_special = get_thick_line(filename,thickness)
+        repeated_line = False
+        for j in lines_data:
+            if j == line_special:
+                repeated_line = True
         
-    if not repeated_line:
-        lines_data.append(line_special)
-
+        if not repeated_line:
+            lines_data.append(line_special)
+    #print(lines_data)
     #lines_data.append(line_format_15)
     data_array_unsort = np.loadtxt(lines_data)
     #sort numpy array by the first collumn
@@ -89,7 +95,8 @@ def gen_latex_table(table,energies):
     print(r"\hline")
     for j in table:
         j = j.replace("$1000.0^{\circ}$", "-")
-        j = j.replace("$-10.0$","-")
+        j = j.replace("$-100.0\%$","-")
+        j = j.replace("$-1.0E+07$","-")
         j = j.replace("$-10000.0$", "-")
         print(j)
     print("\hline\n \end{tabular} \n } \n \end{table}")
@@ -100,8 +107,12 @@ print("Thickness =", thickness)
 
 element_z = 1 #starting at H 
 for element in elements: #Loop in all files of folder
-    filename = "{0}_{1}".format(element, radical)
+    filename = "{0}_alpide.txt".format(element)
+    filename_si = "{0}-Si.txt".format(element)
+    filename_metal = "{0}_Al-Si-O.txt".format(element)
     data_arr = format_file(filename,element)
+    data_si = format_file(filename_si,element,False,23,128)
+    data_metal = format_file(filename_metal,element,False)
     dE_dx = np.add(data_arr[:,1],data_arr[:,2]) # Get sum of colluns 1 e 2 (of dE/dx)
     dE_dx_elec = data_arr[:,1]
     dE_dx_nuc = data_arr[:,2]
@@ -128,13 +139,11 @@ for element in elements: #Loop in all files of folder
         line_nuc = element
         line_multScatt = element
         for i in initial_energy:
-            En_loss_total, theta_slice = calculate_energyLossFraction(dE_dx,range_data,kinEn,i,thickness,element_mass[element_z - 1],radiation_length,weigth, density)
-            En_loss_elec, theta = calculate_energyLossFraction(dE_dx_elec,range_data,kinEn,i,thickness,element_mass[element_z - 1],radiation_length,weigth, density)
-            
-            En_loss_nuc, theta = calculate_energyLossFraction(dE_dx_nuc,range_data,kinEn,i,thickness,element_mass[element_z - 1],radiation_length,weigth, density)
-            line_total += " & ${0:.1f}$".format(En_loss_total)
-            line_elec += " & ${0:.1f}$".format(En_loss_elec)
-            line_nuc += " & ${0:.1f}$".format(En_loss_nuc*1e3) # to keV
+            En_loss_total,En_loss_elec,En_loss_nuc,theta_slice = calculate_energyLossFraction(data_metal,data_si,i,thickness,element_mass[element_z - 1],radiation_length,weigth_metal, weigth_si,density_metal,density_si)
+          
+            line_total += " & ${0:.1f}\%$".format((En_loss_total/i)*100)
+            line_elec += " & ${0:.1E}$".format(En_loss_elec*1e6)
+            line_nuc += " & ${0:.1E}$".format(En_loss_nuc*1e6) # to eV
             line_multScatt += " & ${0:.1f}".format(theta_slice)
             line_multScatt += "^{\circ}$"
             #print("{0} MeV --------------------- {1:.1f} ----------------{2:.1f}------------------- {3:.1f}".format(i,fracEn*100, theta_scat ,theta_slice))
--- SRIM_analysis/energyLoss_complexScatt.py
+++ SRIM_analysis/energyLoss_complexScatt.py
@@ -6,7 +6,7 @@ def getRadiation_length(Xo, weigth, density):
         Parameters: Xo - Radiation length
         weigth - fraction of mass of mixture
         density - density of the material'''
-    n_list = len(Xo)
+    n_list = len(weigth)
     reciprocal_Xo = 0
     for i in range(0,n_list):
         reciprocal_Xo += weigth[i]/Xo[i]
@@ -25,51 +25,63 @@ def calculateBeta(K,E_rest):
 
     xi = (K + E_rest)/E_rest
     return np.sqrt( (xi**2 - 1)/(xi**2) )
+    
 
-def calculate_energyLossFraction(de_dx,range_data,kinEn,energy,thickness,ion_mass,radiation_length,weigth,density,z=1):
-    energy_pos, = np.where(kinEn == energy)
+def calculate_energyLossFraction(data_metal,data_si,energy,thickness,ion_mass,radiation_length,weigth_metal, weigth_si,density_metal,density_si,z=1):
+    #energy_pos, = np.where(kinEn == energy)
     # print(range_data[energy_pos[0]])
-    diff = range_data[energy_pos[0]] - thickness
-    if(diff >= range_data[0]):
-
-
-        #Now interpolate the data of dE/dx vs k
-
-        interp_function = interpolate.interp1d(kinEn[:energy_pos[0] + 1], de_dx[:energy_pos[0] + 1], kind='cubic')
-        '''
-        plt.title("He")
-        plt.plot(kinEn[:energy_pos[0]+1], interp_function(kinEn[:energy_pos[0] + 1]),label='cubic interpol')
-        plt.plot(kinEn[:energy_pos[0] + 1], de_dx[:energy_pos[0]+1],'o',label="SRIM-2013")
-        plt.xlabel("Kinetic Energy(MeV)")
-        plt.ylabel("dE/dx(MeV)")
-        plt.yscale('log')
-        plt.xscale('log')
-        plt.legend()
-        plt.show()
-        '''
-        init=0.0
-        step=1.0
-        E = energy
-        E_final = 0
-        E_rest = ion_mass
-        #calculate Xo mix
-        Xo_mix = getRadiation_length(radiation_length, weigth, density)
-        sum1 = 0
-        sum2 = 0
-        while(init < thickness):
-
+    #diff = range_data[energy_pos[0]] - thickness
+    
+    
+    init=0.0
+    step=1.0
+    E = energy
+    E_final = 0
+    E_nuc = 0 
+    E_elec = 0
+    E_rest = ion_mass
+    sum1 = 0
+    sum2 = 0
+    while(init < thickness):
+        if(E>0):
+            if(init>=10.0): # until 10 um ih the metal stack.
+                kinEn = data_si[:,0]
+                dE_dx = np.add(data_si[:,1],data_si[:,2])
+                dE_dx_elec = data_si[:,1]
+                dE_dx_nuc = data_si[:,2]
+                Xo = getRadiation_length([radiation_length[0]], weigth_si, density_si)
+            else:
+                kinEn = data_metal[:,0]
+                dE_dx = np.add(data_metal[:,1],data_metal[:,2])
+                dE_dx_elec = data_metal[:,1]
+                dE_dx_nuc = data_metal[:,2]
+                Xo = getRadiation_length(radiation_length, weigth_metal, density_metal)
+            
+            interp_function = interpolate.interp1d(kinEn, dE_dx, kind='cubic')
+            interp_function_elec = interpolate.interp1d(kinEn, dE_dx_elec, kind='cubic')
+            interp_function_nuc = interpolate.interp1d(kinEn, dE_dx_nuc, kind='cubic')
             beta = calculateBeta(E,E_rest)
             p = calculateMomentum(E,E_rest)
-            sum1 += ( (step*1e-4)/(Xo_mix) )*( 1/(p*p*beta*beta) )
-            sum2 += ( (step*1e-4)/(Xo_mix) )*( (z**2)/(beta**2) )
+            sum1 += ( (step*1e-4)/(Xo) )*( 1/(p*p*beta*beta) )
+            sum2 += ( (step*1e-4)/(Xo) )*( (z**2)/(beta**2) )
+            try:
+                interp_function(E)
+            except:
+                return -energy, -10, -10, 1000
             E_loss = interp_function(E)*step*(1e-03) #Kev --> MeV
+            E_loss_elec = interp_function_elec(E)*step*(1e-03)
+            E_loss_nuc = interp_function_nuc(E)*step*(1e-03)
             E_final += E_loss
+            E_elec += E_loss_elec
+            E_nuc +=  E_loss_nuc 
             E -= E_loss
-
             init+=step
 
-        theta = 14.1*z*np.sqrt(sum1)*(1 + (1/9)*np.log10(sum2))*57.2957 # to change to degrees
         #frac_finalEn = (E_final/energy)
-        return E_final, theta 
-    else:
-        return -10, 1000
+        else:
+            return -energy, -10, -10, 1000
+    theta = 14.1*z*np.sqrt(sum1)*(1 + (1/9)*np.log10(sum2))*57.2957 # to change to degrees
+    if (E_final > energy):
+        return -energy, -10, -10, 1000
+    else:	
+    	return E_final, E_elec ,E_nuc, theta 
